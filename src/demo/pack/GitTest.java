9469970caaa2f39441ceef6c586cacf8d7bafa01
Agnivo Bhattacharyya <agni05bhattacharyya@gmail.com>
2023-03-29 15:39:25 +0530

    First commit

---
+++ src/demo/pack/GitTest.java
@@ -0,0 +1,14 @@
+package demo.pack;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 23;
+		int j = 15;
+		int sum = i+ j;
+		System.out.println(sum);
+	}
+
+}
