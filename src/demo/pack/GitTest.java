fc0aa68cde7930192b7de6ac3ff6dc1ad7ed7910
Agnivo Bhattacharyya <agni05bhattacharyya@gmail.com>
2023-03-29 17:35:51 +0530

    git demo commit

--- src/demo/pack/GitTest.java
+++ src/demo/pack/GitTest.java
@@ -28,6 +28,8 @@ public class GitTest {
 		test.getSub();
 		KolkataTest kTest = new KolkataTest();
 		kTest.getText();
+		Multiple multiple = new Multiple();
+		multiple.getMultiple();
 	}
 
 }
---
+++ src/demo/pack/Multiple.java
@@ -0,0 +1,14 @@
+package demo.pack;
+
+public class Multiple {
+	
+	public int getMultiple()
+	{
+		int i = 3;
+		int j = 4;
+		int mul = i*j;
+		System.out.println(mul);
+		return mul;
+	}
+	
+}
